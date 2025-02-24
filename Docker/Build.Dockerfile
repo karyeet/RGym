1580ede77e88f2dd78541f34ace95812be5a3567
Kareem <28524112+karyeet@users.noreply.github.com>
2025-02-24 13:58:41 -0800

    build container

--- Docker/Build.Dockerfile
+++ Docker/Build.Dockerfile
@@ -1,16 +1,18 @@
-from debian:bullseye
+FROM debian:bullseye
 
 # Install the required packages
-RUN apt update && apt install -y gcc make bison flex libelf-dev libssl-dev bc git
+RUN apt update && apt install --no-install-recommends -y gcc make bison flex libelf-dev libssl-dev bc git python3 openssl findutils
 
 # clone kernel source
 #RUN git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 COPY ./linux /linux
 
-COPY ./setup.sh /setup.sh
-RUN chmod +x /setup.sh
+COPY ./setup.py /setup.py
+RUN chmod +x /setup.py
+
+RUN git config --global --add safe.directory '*'
 
 RUN mkdir /share
 
-CMD ["/setup.sh"]
-
+#CMD ["/setup.py"]
+ENTRYPOINT ["/setup.py"]
---
+++ Docker/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+import subprocess
+import sys
+import os
+import hashlib
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: setup.py <commit_hash> <cores>')
+        sys.exit(1)
+
+    print('args received', sys.argv)
+
+    commit_hash = sys.argv[1]
+    cores = int(sys.argv[2])
+
+    if(len(commit_hash) < 1):
+        print('No commit hash provided.')
+        sys.exit(1)
+
+    if(int(cores) > os.cpu_count()):
+        print('Number of cores specified is greater than the number of cores available.')
+        cores = os.cpu_count()
+        
+
+    # Change directory to /linux
+    os.chdir('/linux')
+
+    # Checkout the specified git hash
+    print(f'Checking out {commit_hash}')
+    subprocess.run(['git', 'checkout', commit_hash], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # copy config
+    print('Copying .config')
+    subprocess.run(['cp', '/share/.config', '/linux/.config'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # copy patch if patch exists
+    if os.path.exists('/share/.patch'):
+        print('Applying patch')
+        subprocess.run(['git', 'apply', '/share/.patch'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        print('Not patching (patch not found)')
+
+    # fill defaults
+    print('Filling defaults with make olddefconfig')
+    subprocess.run(['make', 'olddefconfig'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Run make with the number of processors
+    #make_process = subprocess.run(['make', f'-j{os.cpu_count()}'], check=True, stdout=subprocess.PIPE, text=True)
+    
+    make_output = []
+    with subprocess.Popen(['make', f'-j{cores}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
+        for line in process.stdout:
+            print(line.decode('utf8'))
+            make_output.append(line.decode('utf8'))
+
+    # Check the output of the make command
+    print('Reading end of output', '\n'.join(make_output[-5:]))
+    if 'Kernel: arch/x86/boot/bzImage is ready' not in '\n'.join(make_output[-5:]):
+        print('Error: Kernel build did not complete successfully.')
+        sys.exit(1)
+
+    # hash config
+    print('Hashing config')
+    config_hash = ''
+    with open('/linux/.config', 'rb', buffering=0) as f:
+        config_hash = hashlib.file_digest(f, 'sha256').hexdigest()[:7]
+
+    # Copy the bzImage to the /share directory with the hash in the filename
+    bzImage_name = f'/share/bzImage-{commit_hash}-{config_hash}'
+    print(f'Copying bzimage to ', bzImage_name)
+    subprocess.run(['cp', '/linux/arch/x86/boot/bzImage', bzImage_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    print('SUCCESS')
+if __name__ == '__main__':
+    main()
